b9c731b95f2974afce5ea082245f274a70f84475
chrisandoep <chrisep8@gmail.com>
2016-07-31 23:15:15 +0700

    Fix travis CI integration

--- app/src/main/java/com/directdev/portal/network/BinusApi.kt
+++ app/src/main/java/com/directdev/portal/network/BinusApi.kt
@@ -7,27 +7,27 @@ import rx.Observable
 
 interface BinusApi {
     @Headers("Referer: https://newbinusmaya.binus.ac.id/newStudent/",
-            "Cookie: PHPSESSID=rii64bhoik5he6lhslh2a93nf6")
+            "Cookie: PHPSESSID=bhuthulu0cc3hqacm9elas96f3")
     @GET("student/profile/profileStudent")
     fun getProfile(): Observable<ResponseBody>
 
     @Headers("Referer: https://newbinusmaya.binus.ac.id/newStudent/",
-            "Cookie: PHPSESSID=rii64bhoik5he6lhslh2a93nf6")
+            "Cookie: PHPSESSID=bhuthulu0cc3hqacm9elas96f3")
     @GET("financial/getFinancialSummary")
     fun getFinances(): Observable<List<FinanceModel>>
 
     @Headers("Referer: https://newbinusmaya.binus.ac.id/newStudent/index.html",
-            "Cookie: PHPSESSID=rii64bhoik5he6lhslh2a93nf6")
+            "Cookie: PHPSESSID=bhuthulu0cc3hqacm9elas96f3")
     @GET("student/class_schedule/classScheduleGetStudentClassSchedule")
     fun getSchedules(): Observable<List<ScheduleModel>>
 
     @Headers("Referer: https://newbinusmaya.binus.ac.id/newstudent/",
-            "Cookie: PHPSESSID=rii64bhoik5he6lhslh2a93nf6")
+            "Cookie: PHPSESSID=bhuthulu0cc3hqacm9elas96f3")
     @POST("newExam/Schedule/getOwnScheduleStudent")
     fun getExam(@Body data: ExamRequestBody): Observable<List<ExamModel>>
 
     @Headers("Referer: https://newbinusmaya.binus.ac.id/newstudent/",
-            "Cookie: PHPSESSID=rii64bhoik5he6lhslh2a93nf6")
+            "Cookie: PHPSESSID=bhuthulu0cc3hqacm9elas96f3")
     @POST("scoring/ViewGrade/getStudentScore/{term}")
     fun getGrades(@Path("term") term: String): Observable<GradeModel>
 }
