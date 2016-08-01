65eadd74ee17fab3df8fc9e6b9f57b276043a362
chrisandoep <chrisep8@gmail.com>
2016-08-01 08:35:56 +0700

    Fix travis CI integration

--- app/src/androidTest/java/com/directdev/portal/NetworkCallTest.kt
+++ app/src/androidTest/java/com/directdev/portal/NetworkCallTest.kt
@@ -28,9 +28,9 @@ class NetworkCallTest {
             //TODO: (NOTE) Delete OkHttpClient if timeout takes too long
             .client(OkHttpClient()
                     .newBuilder()
-                    .connectTimeout(20, TimeUnit.SECONDS)
-                    .readTimeout(20, TimeUnit.SECONDS)
-                    .writeTimeout(20, TimeUnit.SECONDS)
+                    .connectTimeout(30, TimeUnit.SECONDS)
+                    .readTimeout(40, TimeUnit.SECONDS)
+                    .writeTimeout(40, TimeUnit.SECONDS)
                     .addNetworkInterceptor(StethoInterceptor())
                     .build())
             .baseUrl("https://newbinusmaya.binus.ac.id/services/ci/index.php/")
