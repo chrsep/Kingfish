696119a827e8047a1ade988ba41e530c5b1ecdea
Chrisando EP <chrisep8@gmail.com>
2018-05-10 08:02:50 +0700

    Add login button regex.
    Apparently the login button also changes on every login, so hard-coding the value will no longer works.

--- app/src/main/java/com/directdev/portal/network/BimayService.kt
+++ app/src/main/java/com/directdev/portal/network/BimayService.kt
@@ -32,8 +32,7 @@ interface BimayService {
             "User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36")
     @POST("https://binusmaya.binus.ac.id/login/sys_login.php")
     fun signIn2(@Header("Cookie") cookie: String,
-                @FieldMap fields: Map<String, String>,
-                @Field("TURsaUlVNTFKR3d3UnlGdTA5OjE3") button: String = "Login")
+                @FieldMap fields: Map<String, String>)
             : Single<Response<String>>
 
     @Headers("Referer: https://binusmaya.binus.ac.id/newStaff/",
